2a0d48f808b0259c960c2164b27fedcb047dec2c
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-08-07 20:53:15 +0100

    Link.H: added LinkID, generated by unordered Cantor pairing, made data members non-const to allow assignment

--- include/Link.h
+++ include/Link.h
@@ -1,19 +1,28 @@
 #ifndef LINK_H
 #define LINK_H
 
+#include "typedefs.h"
+#include "utils.h"
+
 namespace CostModel {
   class Link {
     private:
-      const unsigned int LATENCY;
-      const double INV_BW;
+      LinkID link_id;
+      unsigned int latency;
+      double inverse_bw;
 
     public:
       Link() = delete;
       Link(unsigned int lat, double inv_bw)
-      : LATENCY(lat), INV_BW(inv_bw) {};
+      : latency(lat), inverse_bw(inv_bw) {};
 
-      unsigned int getLatency() { return LATENCY; }
-      double getInverseBW() { return INV_BW; }
+      void setLinkID(DevID A, DevID B) {
+        link_id = CostModel::unorderedCantor(A, B);
+        return;
+      };
+      LinkID getLinkID() const { return link_id; }
+      unsigned int getLatency() const { return latency; }
+      double getInverseBW() const { return inverse_bw; }
   };
 }
 
