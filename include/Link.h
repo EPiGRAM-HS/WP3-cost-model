a1df18f526dc24a0651cc7ba44fc80b1e532bb75
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-08-09 11:31:31 +0100

    Link.h tentaively added sum operator for links, requires some further thought

--- include/Link.h
+++ include/Link.h
@@ -16,10 +16,19 @@ namespace CostModel {
       Link(unsigned int lat, double inv_bw)
       : latency(lat), inverse_bw(inv_bw) {};
 
+      Link& operator+=(const Link& RHS) {
+        // treating inverse bandwidth (s/B) as summative here...
+        link_id = 0;
+        latency += RHS.latency;
+        inverse_bw += RHS.latency;
+        return *this;
+      }
+      friend Link operator+(Link lhs, const Link& RHS) { return lhs += RHS; }
+
       void setLinkID(DevID A, DevID B) {
-        link_id = CostModel::unorderedCantor(A, B);
+        link_id = unorderedCantor(A, B);
         return;
-      };
+      }
       LinkID getLinkID() const { return link_id; }
       unsigned int getLatency() const { return latency; }
       double getInverseBW() const { return inverse_bw; }
