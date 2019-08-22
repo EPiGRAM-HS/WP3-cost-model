0a264b3066ff524d7d9f0831b6e9aef46360300f
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-08-22 13:09:08 +0100

    Hardware.h: added getter for const reference to device vector

--- include/Hardware.h
+++ include/Hardware.h
@@ -29,6 +29,7 @@ namespace CostModel {
       std::string getDeviceName(const DevID) const;
       unsigned int getNumDevices() const { return num_devices; }
       const Device getDevice(const DevID) const;
+      const std::vector<Device>& getDevices() const { return devices; }
       Topology& getTopology() { return topo; }
   };
 }
