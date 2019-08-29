d190b9cab58b5042a9f3ccdb6641274b4ff4556d
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2019-08-29 11:02:25 +0100

    Access.cpp: corrected size reservation, changed behaviour to promote to cost of access pattern, not just EXPENSIVE

--- src/Access.cpp
+++ src/Access.cpp
@@ -3,30 +3,34 @@
 
 namespace CostModel {
   const AccessPattern Access::unrollAccessPattern(
-    const AccessPattern& in_pattern, const DataLayout& layout) {
-    const AccessPattern& lpattern = layout.getPattern();
+    const AccessPattern& IN_PATTERN, const DataLayout& layout) {
+    const AccessPattern& LPATTERN = layout.getPattern();
+    AccessPattern out_pattern;
+
+    if (LPATTERN.empty() || IN_PATTERN.empty()) return out_pattern;
+
+    const AccessType DTYPE_FIRST = std::get<0>(LPATTERN.front());
 
     size_t unrolled_size = 0;
-    for (auto acc : in_pattern)
-      unrolled_size += std::get<1>(acc) * lpattern.size();
+    for (auto ACC : IN_PATTERN)
+      unrolled_size += std::get<1>(ACC) * LPATTERN.size();
+    out_pattern.reserve(unrolled_size);
 
-    AccessPattern out_pattern(unrolled_size);
-
-    for (auto in_acc : in_pattern) {
-      for (size_t rep = 0; rep < std::get<1>(in_acc); ++rep) {
-        if (std::get<0>(in_acc) < AccessType::EXPENSIVE) {
+    for (auto ACC : IN_PATTERN) {
+      for (size_t rep = 0; rep < std::get<1>(ACC); ++rep) {
+        if (std::get<0>(ACC) <= DTYPE_FIRST) {
           // cost is just that of underlying datatype
-          for (auto acc : lpattern) out_pattern.push_back(acc);
+          for (auto LACC : LPATTERN) out_pattern.push_back(LACC);
         } else {
-          // cost of initial access should be promoted to EXPENSIVE
-          out_pattern.push_back(std::make_pair(AccessType::EXPENSIVE, 1));
+          // cost of initial access should be promoted
+          out_pattern.push_back(std::make_pair(std::get<0>(ACC), 1));
 
-          auto [init_type, init_count] = lpattern.front();
-          if (init_count > 1)
-          out_pattern.push_back(std::make_pair(init_type, init_count - 1));
+          auto [INIT_ACC, INIT_COUNT] = LPATTERN.front();
+          if (INIT_COUNT > 1)
+          out_pattern.push_back(std::make_pair(INIT_ACC, INIT_COUNT - 1));
 
-          for (auto lpat_it = lpattern.begin() + 1; lpat_it != lpattern.end();
-            ++lpat_it) out_pattern.push_back(*lpat_it);
+          for (auto LPAT_IT = LPATTERN.begin() + 1; LPAT_IT != LPATTERN.end();
+            ++LPAT_IT) out_pattern.push_back(*LPAT_IT);
         }
       }
     }
